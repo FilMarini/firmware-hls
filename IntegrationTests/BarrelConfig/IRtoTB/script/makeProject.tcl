4872263bbfc60ebfdcf63e7c881ae9f314f367f1
Andrew Hart <ahart@cern.ch>
2022-05-10 07:43:52 -0400

    Switched to a VU13P.

--- IntegrationTests/BarrelConfig/IRtoTB/script/makeProject.tcl
+++ IntegrationTests/BarrelConfig/IRtoTB/script/makeProject.tcl
@@ -3,7 +3,8 @@
 
 # Create project
 set projName "Work"
-set FPGA "xcvu7p-flvb2104-1-e"
+#set FPGA "xcvu7p-flvb2104-1-e"
+set FPGA "xcvu13p-fhgb2104-1-e"
 create_project -force ${projName} ./${projName} -part $FPGA
 set_property target_language VHDL [current_project]
 
