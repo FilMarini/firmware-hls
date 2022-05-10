2d3d9db88da54b96f8eff1be835e7b7d17ce8ce4
Andrew Hart <ahart@cern.ch>
2022-05-10 07:53:10 -0400

    Added clock frequency parameter.

--- IntegrationTests/common/script/build_ip.tcl
+++ IntegrationTests/common/script/build_ip.tcl
@@ -2,7 +2,7 @@
 # Build an HLS IP #
 ###################
 
-proc build_ip {top_function project_location} {
+proc build_ip {top_function project_location clock_frequency} {
     global exe
     set cwd [pwd]
 
@@ -10,6 +10,7 @@ proc build_ip {top_function project_location} {
     open_solution "solution_$top_function"
     cd $project_location
     source settings_hls.tcl
+    create_clock -period $clock_frequency -name default
     cd $cwd
     csynth_design
     export_design -format ip_catalog
