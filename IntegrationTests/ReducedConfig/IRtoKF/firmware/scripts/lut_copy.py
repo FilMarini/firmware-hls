c190a4d0e73e4a92fc9783ef6425273d14550fa9
FilMarini <marinifil@gmail.com>
2023-03-14 18:48:22 +0100

    xsim works

--- IntegrationTests/ReducedConfig/IRtoKF/firmware/scripts/lut_copy.py
+++ IntegrationTests/ReducedConfig/IRtoKF/firmware/scripts/lut_copy.py
@@ -9,7 +9,16 @@ parser.add_argument("-s", "--pre_synth_file", dest="preSynthPath", help="pre-syn
 # Parse arguments
 args = parser.parse_args()
 
-with open(args.preSynthPath, "a") as presynth_file:
-    presynth_file.write("\n# Copy LUTs to xsim or synth_1 directory")
-    presynth_file.write("\nfile delete -force ./LUTs")
-    presynth_file.write("\nfile copy -force ../../../../../src/firmware-hls/emData/LUTsReduced ./LUTs")
+# Already added the line?
+with open(args.preSynthPath) as psf:
+    for line in psf:
+        pass
+    last_line = line
+
+# Add lines
+if (last_line != "# LUTs added"):
+    with open(args.preSynthPath, "a") as presynth_file:
+        presynth_file.write("\n# Copy LUTs to xsim or synth_1 directory")
+        presynth_file.write("\nfile delete -force ./LUTs")
+        presynth_file.write("\nfile copy -force ../../../../../src/firmware-hls/emData/LUTsReduced ./LUTs")
+        presynth_file.write("\n# LUTs added")
--- IntegrationTests/ReducedConfig/IRtoKF/firmware/ucf/vsim.tcl
+++ IntegrationTests/ReducedConfig/IRtoKF/firmware/ucf/vsim.tcl
@@ -1,6 +1,12 @@
 set_property SOURCE_SET sources_1 [get_filesets sim_1]
-add_files -fileset sim_1 -norecurse ../../src/firmware-hls/firmware/mem/in.txt
+add_files -fileset sim_1 -norecurse ../../src/firmware-hls/IntegrationTests/ReducedConfig/IRtoKF/firmware/mem/in.txt
+set lut_script [file dirname [file normalize ../../src/firmware-hls/IntegrationTests/ReducedConfig/IRtoKF/firmware/scripts/pre_msim.tcl ]]
+append lut_script "/pre_msim.tcl"
+add_files -fileset utils_1 -norecurse $lut_script
+#set_property STEPS.SYNTH_DESIGN.TCL.PRE $lut_script [get_runs sim_1]
 #add_files -fileset sim_1 -norecurse ../../src/firmware-hls/firmware/mem/out.txt
+set pre_path "set_property -name {xsim.compile.tcl.pre} -value {$lut_script} -objects \[\get_filesets \sim_1\]"
+eval ${pre_path}
 
 # Disabling source management mode.  This is to allow the top design properties to be set without GUI intervention.
 set_property source_mgmt_mode None [current_project]
