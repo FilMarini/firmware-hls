0beecff3c4bcc1166354455e6d1252c0fe2049dc
FilMarini <marinifil@gmail.com>
2023-03-14 14:38:01 +0100

    changed lut copy

---
+++ IntegrationTests/ReducedConfig/IRtoKF/firmware/scripts/lut_copy.py
@@ -0,0 +1,15 @@
+import argparse
+
+# Create argument parser
+parser = argparse.ArgumentParser(description="Add pre synth instructions.")
+
+# Optional arguments
+parser.add_argument("-s", "--pre_synth_file", dest="preSynthPath", help="pre-synth file", type=str, default="../../../../../emp-fwk/components/info/firmware/cfg/set_generics_presynth.tcl")
+
+# Parse arguments
+args = parser.parse_args()
+
+with open(args.preSynthPath, "a") as presynth_file:
+    presynth_file.write("\n# Copy LUTs to xsim or synth_1 directory")
+    presynth_file.write("\nfile delete -force ./LUTs")
+    presynth_file.write("\nfile copy -force ../../../../../src/firmware-hls/emData/LUTsReduced ./LUTs")
